0dca91daf6f1ccd96d6083bbb123cda274225831
Diego Faccipieri <diegofaccipieri@macbookpro.home>
2026-03-09 15:16:02 -0400

    feat: global hotkey Cmd+Shift+Space and dynamic menu bar icon

--- DictationApp/AppDelegate.swift
+++ DictationApp/AppDelegate.swift
@@ -1,12 +1,17 @@
 import AppKit
+import Combine
+import KeyboardShortcuts
 import SwiftUI
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem?
     var popover: NSPopover?
+    let viewModel = DictationViewModel()
+    private var cancellables = Set<AnyCancellable>()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         setupMenuBar()
+        setupHotkey()
     }
 
     func setupMenuBar() {
@@ -18,12 +23,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             button.target = self
         }
 
-        let contentView = ContentView(viewModel: DictationViewModel())
+        let contentView = ContentView(viewModel: viewModel)
         let popover = NSPopover()
-        popover.contentSize = NSSize(width: 360, height: 240)
+        popover.contentSize = NSSize(width: 360, height: 260)
         popover.behavior = .transient
         popover.contentViewController = NSHostingController(rootView: contentView)
         self.popover = popover
+
+        viewModel.$state
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] state in
+                let iconName = state == .recording ? "mic.fill" : "mic"
+                self?.statusItem?.button?.image = NSImage(systemSymbolName: iconName, accessibilityDescription: "Dictation")
+                self?.statusItem?.button?.contentTintColor = state == .recording ? .systemRed : nil
+            }
+            .store(in: &cancellables)
+    }
+
+    func setupHotkey() {
+        KeyboardShortcuts.onKeyUp(for: .toggleDictation) { [weak self] in
+            Task { @MainActor in
+                self?.viewModel.toggle()
+            }
+        }
     }
 
     @objc func togglePopover() {
---
+++ DictationApp/HotkeyManager.swift
@@ -0,0 +1,5 @@
+import KeyboardShortcuts
+
+extension KeyboardShortcuts.Name {
+    static let toggleDictation = Self("toggleDictation", default: .init(.space, modifiers: [.command, .shift]))
+}
