3550c62a1c61f2ae816866e019c90d2d5c102cc5
Diego Faccipieri <diegofaccipieri@macbookpro.home>
2026-03-09 15:10:23 -0400

    feat: menu bar icon and popover skeleton

--- DictationApp/AppDelegate.swift
+++ DictationApp/AppDelegate.swift
@@ -2,6 +2,36 @@ import AppKit
 import SwiftUI
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    var popover: NSPopover?
+
     func applicationDidFinishLaunching(_ notification: Notification) {
+        setupMenuBar()
+    }
+
+    func setupMenuBar() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem?.button {
+            button.image = NSImage(systemSymbolName: "mic", accessibilityDescription: "Dictation")
+            button.action = #selector(togglePopover)
+            button.target = self
+        }
+
+        let contentView = ContentView()
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 360, height: 240)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+    }
+
+    @objc func togglePopover() {
+        guard let button = statusItem?.button else { return }
+        if popover?.isShown == true {
+            popover?.performClose(nil)
+        } else {
+            popover?.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+        }
     }
 }
